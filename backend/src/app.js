96e484e7fc66855d26c48f8bad69b5dc55723ab0
Vishal Subhash Rathod <154747312+vishalsr20@users.noreply.github.com>
2025-12-11 21:17:44 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://zomareel-com.onrender.com",
     credentials: true
 }))
 
